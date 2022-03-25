e2c24c7cd9e38aa37c8f364b6580fd08be5c0b8a
Jozef Pavlík <pavlik.jozef.uniza@gmail.com>
2022-03-25 10:20:45 +0100

    Week 3 api folder added with v1 & middlewares, routing logic, get.patients etc.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 import config from 'config'
+import http from 'http'
+import app from './app'
 
+/*
 function f() {
 	console.log("Hello world")
 }
 
 f()
+*/
+
+const httpServer = http.createServer(app)
+const serverConfig: { port: number } = config.get('server')
+
+httpServer.listen(serverConfig.port).on('listening', () => {
+	console.log(`Server started on port ${serverConfig.port}`)
+})
