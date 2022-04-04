d6541ba9099ee696879eab0f4f8ea4d2f1f6622d
Jozef Pavlík <pavlik.jozef.uniza@gmail.com>
2022-04-04 16:46:37 +0200

    Created substances model with all params and export function .init

--- src/db/models/substances.ts
+++ src/db/models/substances.ts
@@ -1,5 +1,49 @@
 import { Sequelize, Model, DataTypes } from 'sequelize'
+import { Models } from '..'
+import { SUBSTANCE_TIME_UNIT, SUBSTANCE_TIME_UNITS } from '../../utils/enums'
 
 export class SubstanceModel extends Model {
     id: number
+    name: string
+    timeUnit: SUBSTANCE_TIME_UNIT
+    halfLife: number
+}
+
+export default (sequelize: Sequelize, modelName: string) => {
+	SubstanceModel.init(
+		{
+			id: {
+				type: DataTypes.BIGINT,
+				allowNull: false,
+				primaryKey: true,
+				unique: true,
+				autoIncrement: true,
+			},
+			name: {
+				type: DataTypes.TEXT,
+				allowNull: false,
+			},
+			timeUnit: {
+				type: DataTypes.ENUM(...SUBSTANCE_TIME_UNITS),
+				allowNull: false,
+			},
+			halfLife: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+			},
+		},
+		{
+			paranoid: false,
+			timestamps: false,
+			sequelize,
+			modelName,
+			tableName: 'diagnoses',
+		}
+	);
+
+	(SubstanceModel as any).associate = (models: Models) => {
+		SubstanceModel.belongsTo(models.Diagnose, { foreignKey: 'substanceID' })
+	}
+
+	return SubstanceModel
 }
