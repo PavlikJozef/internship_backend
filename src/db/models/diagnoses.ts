d760c4d00a72f36e62a24579ee0304b166639c07
Jozef Pavlík <pavlik.jozef.uniza@gmail.com>
2022-04-04 16:45:48 +0200

    Created diagnose model with all params and export function .init

--- src/db/models/diagnoses.ts
+++ src/db/models/diagnoses.ts
@@ -1,5 +1,56 @@
 import { Sequelize, Model, DataTypes } from 'sequelize'
+import { Models } from '..'
+import { PatientModel } from './patients'
+import { SubstanceModel } from './substances'
 
-export class DiagnosetModel extends Model {
+export class DiagnoseModel extends Model {
     id: number
+    name: string
+    description: string
+    
+    // foreign keys
+    substanceID: number
+    substance: SubstanceModel
+    patients: PatientModel[]
+}
+
+export default (sequelize: Sequelize, modelName: string) => {
+	DiagnoseModel.init(
+		{
+			id: {
+				type: DataTypes.BIGINT,
+				allowNull: false,
+				primaryKey: true,
+				unique: true,
+				autoIncrement: true,
+			},
+			name: {
+				type: DataTypes.TEXT,
+				allowNull: false,
+			},
+			description: {
+				type: DataTypes.TEXT,
+				allowNull: false,
+			},
+			// foreign keys
+			substanceID: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+			},
+		},
+		{
+			paranoid: false,
+			timestamps: false,
+			sequelize,
+			modelName,
+			tableName: 'diagnoses',
+		}
+	);
+
+	(DiagnoseModel as any).associate = (models: Models) => {
+		DiagnoseModel.belongsTo(models.Substance, { foreignKey: 'substanceID' })
+		DiagnoseModel.hasMany(models.Patient, { foreignKey: 'diagnoseID' })
+	}
+
+	return DiagnoseModel
 }
