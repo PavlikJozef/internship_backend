d2e558072a6ae1ed6eddace4a5610af83453dedf
Jozef Pavlík <pavlik.jozef.uniza@gmail.com>
2022-04-04 16:44:25 +0200

    included definition for substance and diagnose according patients model into index.ts in db folder

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,8 @@
 import { Sequelize } from "sequelize"
 import * as database from '../../config/database'
 
-//import defineSubstance from './models/substances'
-//import defineDiagnose from './models/diagnoses'
+import defineSubstance from './models/substances'
+import defineDiagnose from './models/diagnoses'
 import definePatient from './models/patients'
 import { forEach } from 'lodash'
 
@@ -16,8 +16,8 @@ sequelize.authenticate()
             .catch((err) => console.log(`Unable to connect to database ${err.messages}`))
 
 const modelsBuilder = (instance: Sequelize) => ({
-    //Substance: defineSubstance(instance, 'substance'),
-    //Diagnose: defineDiagnose(instance, 'diagnose'),
+    Substance: defineSubstance(instance, 'substance'),
+    Diagnose: defineDiagnose(instance, 'diagnose'),
     Patient: definePatient(instance, 'patient')
 
 })
