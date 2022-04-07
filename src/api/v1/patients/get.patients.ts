038a9a5dadda30973f18dae6d1920ba8a0a2558f
Jozef Pavlík <pavlik.jozef.uniza@gmail.com>
2022-04-07 13:58:49 +0200

    Delete and get all patients modifed, get patients extended by showing also substance and diagnose data of patient

--- src/api/v1/patients/get.patients.ts
+++ src/api/v1/patients/get.patients.ts
@@ -1,13 +1,23 @@
 import { Request, Response } from 'express'
 import { models } from '../../../db'
+import { DiagnoseModel } from '../../../db/models/diagnoses'
 import { PatientModel } from '../../../db/models/patients'
+import { SubstanceModel } from '../../../db/models/substances'
 
 export const workflow = async (req: Request, res: Response) => {
     const {
         Patient
     } = models 
 
-    const patients: PatientModel[] = await Patient.findAll()
+    const patients: PatientModel[] = await Patient.findAll({
+        include: {
+        model: DiagnoseModel,
+            include: [{
+                model: SubstanceModel
+            }]
+        }
+    }
+)
 
     res.json({
         patients
